7f6f878430df3158b23e3c24028d0d242cfce849
Jair De La Rosa <yairdlrv@gmail.com>
2025-01-07 13:44:21 -0500

    + beta

--- src/functions/axios.js
+++ src/functions/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const BASE_URL = 'https://apirest-variedadesbyj.onrender.com'; 
+const BASE_URL = 'https://apirest-variedadesbyj.onrender.com/byj'; 
 
 
 export const fetchData = async (endpoint) => {
